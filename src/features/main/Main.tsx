b813581e123735645bd1f7ea40dafbbaadf1b2f9
Harold C <hcabalic@gmail.com>
2023-01-17 14:28:08 -0800

    Add pager - styling & click logic

--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -2,62 +2,65 @@ import { render } from '@testing-library/react';
 import React, { useState, useEffect, ReactText } from 'react';
 import { Movie } from '../../types/Movie';
 import './Main.scss';
+import { handlePageClick, handleSliderClick } from './MainUtils';
 
 export function Main() {
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
   const [isActive, setIsActive] = useState(false);
   const [activeIndex, setActiveIndex] = useState(-1);
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=ddb9cdf5d7e5e833c1ace354ee4baa49&language=en-US&page=1')
     .then(response => response.json())
-    .then(res => setNowPlaying(res.results))
+    .then(res => {
+      setNowPlaying(res.results);
+      setPageCount(res.results.length / 4)
+    })
   }, [])
-
-  const handleSliderClick = (e: React.MouseEvent) => {
-    const slider = (e.target as HTMLElement).closest(".movie-cards__container")?.querySelector(".movie-cards__slider");
-    if (slider) {
-      const sliderIndex = parseInt(window.getComputedStyle(slider).getPropertyValue("--slider-index"));
-      if ((e.target as HTMLElement).classList.contains("movie-cards__scroll--left")) {
-        (slider as HTMLElement).style.setProperty("--slider-index", (sliderIndex - 1).toString());
-      } else {
-        (slider as HTMLElement).style.setProperty("--slider-index", (sliderIndex + 1).toString());
-      }
-    }
-  }
 
   return (
     <div className="movie-cards__wrapper">
       <h2>Main</h2>
-      <h3 className="movie-category__header">Now In Theaters</h3>
-      <div className="movie-cards__container">
-        <div className="movie-cards__scroll movie-cards__scroll--left" onClick={handleSliderClick}>LEFT</div>
-        <div className="movie-cards__slider">
-          { 
-            nowPlaying.map((item: Movie, i: number) => 
-              <div key={i} className={(i == activeIndex && isActive) ? "movie-card movie-card--active" : "movie-card"} 
-                onMouseOver={() => {
-                  setActiveIndex(i);
-                  setIsActive(true);
-                }} 
-                onMouseOut={() => {
-                  setActiveIndex(-1);
-                  setIsActive(false);                
-                }}>
-                <img src={"https://image.tmdb.org/t/p/original" + item.poster_path} alt={"Poster for " + item.title}/>
-                <div className="movie-card__details">
-                  <p>Preview here</p>
-                  <div className="movie-card__info">
-                    <h4>{item.title}</h4>
-                    <ul>
-                      <li>Popularity: {item.popularity}%</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>)
-          }
+      <div className="movie-cards__group-container">
+        <div className="movie-cards__header">
+          <h3 className="movie-cards__category">Now In Theaters</h3>
+          <div className="movie-cards__pages">
+            {
+              [...Array(pageCount)].map((element, i) => <div className={i == 0 ? "movie-cards__pages-item movie-cards__pages-item--active" : "movie-cards__pages-item"} data-page-key={i} key={i} onClick={handlePageClick}></div>)
+            }
+          </div>
         </div>
-        <div className="movie-cards__scroll movie-cards__scroll--right" onClick={handleSliderClick}>right</div>
+        <div className="movie-cards__slider-container">
+          <div className="movie-cards__scroll movie-cards__scroll--left" onClick={handleSliderClick}>&lsaquo;</div>
+          <div className="movie-cards__slider">
+            { 
+              nowPlaying.map((item: Movie, i: number) => 
+                <div key={i} className={(i == activeIndex && isActive) ? "movie-card movie-card--active" : "movie-card"} 
+                  onMouseOver={() => {
+                    setActiveIndex(i);
+                    setIsActive(true);
+                  }} 
+                  onMouseOut={() => {
+                    setActiveIndex(-1);
+                    setIsActive(false);                
+                  }}>
+                  <img src={"https://image.tmdb.org/t/p/original" + item.poster_path} alt={"Poster for " + item.title}/>
+                  <div className="movie-card__details">
+                    <p>Preview here</p>
+                    <div className="movie-card__info">
+                      <h4>{item.title}</h4>
+                      <ul>
+                        <li>Popularity: {item.popularity}%</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>)
+            }
+          </div>
+          <div className="movie-cards__scroll movie-cards__scroll--right" onClick={handleSliderClick}>&rsaquo;</div>
+        </div> 
       </div>
+      
     </div>
   )
 }
---
+++ src/features/main/MainUtils.ts
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const handleSliderClick = (e: React.MouseEvent) => {
+  const target = e.target as HTMLElement;
+  const slider = target.closest(".movie-cards__group-container")?.querySelector(".movie-cards__slider");
+  if (slider) {
+    let sliderIndex = parseInt(window.getComputedStyle(slider).getPropertyValue("--slider-index"));
+    if (target.classList.contains("movie-cards__scroll--left")) {
+      sliderIndex = sliderIndex - 1;
+    } else {
+      sliderIndex = sliderIndex + 1;
+    }
+    (slider as HTMLElement).style.setProperty("--slider-index", (sliderIndex).toString());
+
+    // also update pager
+    const pagesGroup = target.closest(".movie-cards__group-container")?.querySelector(".movie-cards__pages");
+    if (pagesGroup) {
+      pagesGroup.querySelector('.movie-cards__pages-item--active')?.classList.remove('movie-cards__pages-item--active');
+      pagesGroup.querySelectorAll("[data-page-key='" + sliderIndex.toString() + "']")[0].classList.add("movie-cards__pages-item--active");
+    }
+  }
+}
+
+export const handlePageClick = (e: React.MouseEvent<HTMLDivElement>) => {
+  const target = e.target as HTMLElement;
+  const clickedItemIndex = target.getAttribute('data-page-key');
+  if (clickedItemIndex) {
+    const targetPage = parseInt(clickedItemIndex);
+    target.closest(".movie-cards__pages")?.querySelector(".movie-cards__pages-item--active")?.classList.remove("movie-cards__pages-item--active");
+    target.classList.add("movie-cards__pages-item--active");
+    const slider = target.closest(".movie-cards__group-container")?.querySelector(".movie-cards__slider");
+    (slider as HTMLElement).style.setProperty("--slider-index", (targetPage).toString());
+  }
+}
