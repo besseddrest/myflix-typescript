89122e23f7f78287fdeda0515c5c7ed175e0d0a5
Harold C <hcabalic@gmail.com>
2023-01-17 00:49:44 -0800

    Container spacing and add left right scroll elements

--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -13,43 +13,42 @@ export function Main() {
     .then(res => setNowPlaying(res.results))
   }, [])
 
-  const handleMouseOver = (e: React.MouseEvent) => {
+  const handleMouseClick = (e: React.MouseEvent) => {
     console.log(e.target);
-    setIsActive(true);
-  }
-
-  const handleMouseOut = (e: React.MouseEvent) => {
-    setIsActive(false);
   }
 
   return (
-    <div>
+    <div className="movie-cards__wrapper">
       <h2>Main</h2>
-      <h3>Now In Theaters</h3>
-      <div className="movie__card-container">
-        { 
-          nowPlaying.map((item: Movie, i: number) => 
-            <div key={i} className={(i == activeIndex && isActive) ? "movie__card movie__card--active" : "movie__card"} 
-              onMouseOver={() => {
-                setActiveIndex(i);
-                setIsActive(true);
-              }} 
-              onMouseOut={() => {
-                setActiveIndex(-1);
-                setIsActive(false);                
-              }}>
-              <img src={"https://image.tmdb.org/t/p/original" + item.poster_path} />
-              <div className="movie__card-details">
-                <p>Preview here</p>
-                <div className="movie__card-info">
-                  <h4>{item.title}</h4>
-                  <ul>
-                    <li>Popularity: {item.popularity}%</li>
-                  </ul>
+      <h3 className="movie-category__header">Now In Theaters</h3>
+      <div className="movie-cards__container">
+        <div className="movie-cards__scroll movie-cards__scroll--left" onClick={handleMouseClick}>LEFT</div>
+        <div className="movie-cards__slider">
+          { 
+            nowPlaying.map((item: Movie, i: number) => 
+              <div key={i} className={(i == activeIndex && isActive) ? "movie-card movie-card--active" : "movie-card"} 
+                onMouseOver={() => {
+                  setActiveIndex(i);
+                  setIsActive(true);
+                }} 
+                onMouseOut={() => {
+                  setActiveIndex(-1);
+                  setIsActive(false);                
+                }}>
+                <img src={"https://image.tmdb.org/t/p/original" + item.poster_path} alt={"Poster for " + item.title}/>
+                <div className="movie-card__details">
+                  <p>Preview here</p>
+                  <div className="movie-card__info">
+                    <h4>{item.title}</h4>
+                    <ul>
+                      <li>Popularity: {item.popularity}%</li>
+                    </ul>
+                  </div>
                 </div>
-              </div>
-            </div>)
-        }
+              </div>)
+          }
+        </div>
+        <div className="movie-cards__scroll movie-cards__scroll--right" onClick={handleMouseClick}>right</div>
       </div>
     </div>
   )
