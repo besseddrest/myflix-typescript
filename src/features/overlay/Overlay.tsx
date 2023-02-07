1b742b9a190265b4406979c5c52a433c9ef93f63
Harold C <hcabalic@gmail.com>
2023-02-07 11:44:11 -0800

    fix extra scroll issue

--- src/features/overlay/Overlay.tsx
+++ src/features/overlay/Overlay.tsx
@@ -27,17 +27,28 @@ export const Overlay: React.FC<OverlayProps> = (props) => {
           <button className="button--icon">&#65291;</button>
           <button className="button--icon">&#128077;</button>
         </div>
-        <div className="movie__ratings"><strong>Score: {movieDetails.popularity}</strong> . {getYear(movieDetails.release_date)} . Avg: {movieDetails.vote_average} . {formatRuntime(movieDetails.runtime)}</div>
+        <div className="movie__ratings"><strong>Score: {movieDetails.popularity}</strong> &#x2022; {getYear(movieDetails.release_date)} &#x2022; Avg: {movieDetails.vote_average} &#x2022; {formatRuntime(movieDetails.runtime)}</div>
         <div className="movie__info">
           <div><p>{movieDetails.overview}</p></div>
           <div className="movie_sidebar">
             <p><strong>Cast: </strong>
-              {movieDetails.cast.map((name, i) => <span key={i}>{name}{(i < movieDetails.cast.length - 1) ? ', ' : ' '}</span>)}
+              {movieDetails.cast.map((name, i) => <span key={i}>{name}{
+                (i < movieDetails.cast.length - 1) 
+                  ? ', ' 
+                  : <span>&nbsp;<a>more...</a></span>}</span>
+                )
+              }
             </p>
             <p><strong>Genres: </strong>
               {movieDetails.genres.map((genre, i) => <span key={i}>{genre.name}{(i < movieDetails.genres.length - 1) ? ', ' : ' '}</span>)}
             </p>
           </div>
+        </div>
+        <div>
+          <h4>About {movieDetails.title}</h4>
+          <p>
+            about info here
+          </p>
         </div>
       </div>}
       <button className="button--icon-close button--icon" onClick={() => handleOverlayClose()}>&#10006;</button>
